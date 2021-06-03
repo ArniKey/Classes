5b2ac12afd1d214c54f545f23c85642b2344d602
ArniKey <arnau.sellwood@gmail.com>
2021-06-03 19:24:54 +0200

    added function to dates.py project

---
+++ dates.py
@@ -0,0 +1,13 @@
+def es_bixest(any_bixest):
+    if any_bixest % 4 == 0:
+        if any_bixest % 100 == 0:
+            if any_bixest % 400 == 0:
+                return 1
+            else:
+                returngi 0
+        else:
+            return 1
+    else:
+        return 0
+
+print(es_bixest(1001))
