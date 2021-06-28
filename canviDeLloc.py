d1be78adaf00e98fdf7e7b9ed8e0eb85ecfbd3d2
ArniKey <arnau.sellwood@gmail.com>
2021-06-28 10:44:51 +0200

    added funcion to swap integers

--- canviDeLloc.py
+++ canviDeLloc.py
@@ -1,11 +1,11 @@
 def different_place(lloc1, lloc2):
-    places = [10, 20]
-    places[0] = lloc1
-    places[1] = lloc2
+    places = []
+    places.append(lloc1)
+    places.append(lloc2)
     print(places)
     places.reverse()
     print(places)
 
 
-different_place(10, 20)
+different_place(31241241, 431256574)
 
